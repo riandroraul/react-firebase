6a465ed2d3ba9c39c802558bf763fd054cd7b22b
riandroraul <riandroraul@gmail.com>
2022-12-31 17:43:00 +0700

    first commit

---
+++ src/config/firebase/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxynsh6jZVj-34qluHTb_OjwWBpqgHWOM",
+  authDomain: "simple-notes-firebase-c9ad9.firebaseapp.com",
+  projectId: "simple-notes-firebase-c9ad9",
+  storageBucket: "simple-notes-firebase-c9ad9.appspot.com",
+  messagingSenderId: "476575360600",
+  appId: "1:476575360600:web:082dceb27f7c05275d0c2d",
+  measurementId: "G-TML76F97KJ",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default app;
