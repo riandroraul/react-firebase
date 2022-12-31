0b929933c89138ee88292114c5693a932b1f20d7
riandroraul <riandroraul@gmail.com>
2022-12-31 19:21:55 +0700

    update

---
+++ src/containers/pages/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <p>Dashboard page</p>
+        <button>Go to register</button>
+        <button>Go to login</button>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/containers/pages/Register/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import "./register.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+class Register extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChangeText = (el) => {
+    this.setState({
+      [el.target.id]: el.target.value,
+    });
+  };
+
+  handleSubmit = () => {
+    const { email, password } = this.state;
+    console.log(email);
+    console.log(password);
+    // const auth = getAuth();
+    // console.log(auth);
+    createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+
+  render() {
+    return (
+      <div className="auth-container">
+        <div className="auth-card">
+          <p className="auth-title">Register page</p>
+          <input
+            className="input"
+            type="email"
+            placeholder="email"
+            name="email"
+            id="email"
+            onChange={this.handleChangeText}
+          />
+          <input
+            className="input"
+            type="password"
+            placeholder="password"
+            name="password"
+            id="password"
+            onChange={this.handleChangeText}
+          />
+          <button onClick={this.handleSubmit} className="btn">
+            Register
+          </button>
+        </div>
+        {/* <button>Go to dashboard</button> */}
+      </div>
+    );
+  }
+}
+
+export default Register;
